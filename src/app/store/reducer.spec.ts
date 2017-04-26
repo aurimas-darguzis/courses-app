36679b38142ce2f79d293d5c5bfa67d1b39d5a5d
aurimas-darguzis <aurimas@darguzis.com>
2017-04-26 14:50:46 +0100

    tested initial state of the reducer

---
+++ src/app/store/reducer.spec.ts
@@ -0,0 +1,11 @@
+import { reducer } from './reducer';
+import { FILTER_COURSES } from './../courses/course.actions';
+
+describe('Reducer', () => {
+  it('should have the correct initial state', () => {
+    const state = reducer(undefined, {});
+
+    expect(state.courses.length).toBe(0);
+    expect(state.filteredCourses.length).toBe(0);
+  })
+})
